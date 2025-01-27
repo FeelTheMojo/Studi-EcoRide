596b79eb84fb538f6462168d6d2ab88f2fcd5dd9
FeelTheMojo <ismaelvitalien@gmail.com>
2025-01-27 16:21:17 +0100

    Projet ECF

--- index.php
+++ index.php
@@ -1,8 +1,55 @@
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+</html>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EcoRide-Covoiturage</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
 
+  
+<body>
+
+
+    <!--header-->
+    <?php
+    include("template/header.html");
+    ?>
+        
+
+    <!--main-->
+    
+    <?php
+    
+    $uri = $_SERVER["REQUEST_URI"];
+
+    if ($uri == "/"){
+    include("pages/accueil.php");
+    }
+    else if ($uri == "/inscription"){
+    include("pages/inscription.php");
+    }
+    else if ($uri == "/connexion"){
+    include("pages/connexion.php");
+    }
+    else if ($uri == "/contact"){
+    include("pages/contact.php");
+    }
+    else if ($uri == "/covoiturages"){
+    include("pages/selection-covoiturages.php");
+    }
+    else if ($uri == "/resume"){
+    include("pages/resume-covoiturage.php");
+    }
+    ?>
+        
+
+        
+    <!--footer-->
+    <?php
+    include("template/footer.html");
+    ?>
+
+</body>
 </html>
